685a1d52eb59890e61f833dd4fe1a521a24e2ccc
Joan Reig Roman <joan@MacBook-MacBook-Pro-de-Joan.local>
2020-11-26 21:09:37 +0100

    Chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,22 +1,27 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
-if(!params.has('nombre') || !params.has('sala')){
-    window.location='index.html';
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
 
-var usuario={
+var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(resp){
-        console.log('Usuarios conectados',resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -27,8 +32,9 @@ socket.on('disconnect', function() {
 });
 
 
-// // Enviar información
+// Enviar información
 // socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -36,20 +42,19 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
-// cuando un usuario entra o sale del Chat
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
-socket.on('mensajePrivado', function(mensaje){
-    console.log('Mensaje privado: ', mensaje);
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
